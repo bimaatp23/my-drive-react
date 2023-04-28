03e5222756223714e67eb6985ed2acb03aeea0cf
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-28 23:17:44 +0700

    Add react-router-dom library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './components/dashboard/Index'
+import Login from './components/login/Index'
+import Profile from './components/profile/Index'
 
 function App() {
   return <>
-    <h1 className='text-xl'>Hello World Tailwind CSS</h1>
+    <h1 className='text-2xl'>Hello World Tailwind CSS</h1>
+    <ul>
+      <li><a href="/">Dashboard</a></li>
+      <li><a href="/login">Login</a></li>
+      <li><a href="/profile">Profile</a></li>
+    </ul>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   </>
 }
 
