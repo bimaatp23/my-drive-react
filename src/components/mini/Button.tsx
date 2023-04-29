0a8544d8e6ff53258a804e23b286c5847156eed3
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-29 22:03:45 +0700

    Add button component

---
+++ src/components/mini/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface Props {
+    onclick?: any
+    label: string
+    class?: string
+    type?: 'button' | 'submit' | 'reset' | undefined
+    isLoading?: boolean
+}
+
+export default function Button(props: Props) {
+
+    return <>
+        <button 
+            type={props.type} 
+            onClick={props.onclick} 
+            className={'bg-cyan-500 text-white rounded-md font-semibold hover:bg-cyan-600 ' + (props.class == undefined ? '' : props.class)}
+        >
+            {props.isLoading ? <AnimationLoading/> : props.label}
+        </button>
+    </>
+}
+
+function AnimationLoading() {
+    return <>
+        <p
+            className='animate-pulse'
+        >
+            Loading...
+        </p>
+    </>
+}
