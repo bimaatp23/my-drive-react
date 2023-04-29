3e5fffd4d1f9007ebe5d082f6c76a39ae2ffd1b3
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-29 22:11:21 +0700

    Layouting register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Dashboard from './components/dashboard/Index'
 import Login from './components/login/Index'
 import Profile from './components/profile/Index'
+import Register from './components/register/Index'
 
 function App() {
   return <>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
         <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
---
+++ src/components/register/Index.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Button from '../mini/Button'
+import Input from '../mini/Input'
+
+interface Props {
+
+}
+
+interface State {
+    error: boolean
+    isLoading: boolean
+}
+
+export default class Index extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            error: false,
+            isLoading: false,
+        }
+    }
+    render() {
+        return <>
+            <form 
+                className='fixed left-1/2 -translate-x-1/2 bg-white top-1/2 -translate-y-1/2 w-1/3 flex flex-col items-center rounded-lg py-10 space-y-3'
+            >
+                <h1 
+                    className='text-2xl font-semibold text-cyan-500'
+                >
+                    Register Page
+                </h1>
+                <Input 
+                    type='text' 
+                    placeholder='First Name' 
+                    class='w-2/3 px-4 py-2' 
+                />
+                <Input 
+                    type='text' 
+                    placeholder='Last Name' 
+                    class='w-2/3 px-4 py-2' 
+                />
+                <Input 
+                    type='text' 
+                    placeholder='Email' 
+                    class='w-2/3 px-4 py-2' 
+                />
+                <Input 
+                    type='password' 
+                    placeholder='Password' 
+                    class='w-2/3 px-4 py-2'
+                />
+                <Input 
+                    type='password' 
+                    placeholder='Re-Password' 
+                    class='w-2/3 px-4 py-2'
+                />
+                <Button 
+                    type='submit' 
+                    label='Register' 
+                    class='w-2/3 py-2' 
+                />
+                <p
+                    className='font-semibold'
+                >
+                    Have an account? 
+                    <a 
+                        href='/login'
+                        className='ml-1 text-cyan-500 hover:text-cyan-600'
+                    >
+                        Login
+                    </a>
+                </p>
+            </form>
+        </>
+    }
+}
