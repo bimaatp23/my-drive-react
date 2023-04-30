9fecb67c4a42080e79244fc07daaaba150d33ecb
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-30 13:49:19 +0700

    Layouting header

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import Button from '../mini/Button'
 
 export default function Header() {
     return <>
         <div 
             className='bg-white rounded-lg w-full absolute h-16'
         >
-            Header
+            <div
+                className='flex justify-between px-5 py-4'
+            >   
+                <a 
+                    href="/"
+                >
+                    <h1
+                        className='text-2xl font-bold text-cyan-500'
+                    >
+                        My Drive
+                    </h1>
+                </a>
+                <div
+                    className='space-x-2'
+                >
+                    <Button
+                        label='Profile'
+                        class='px-4 py-1'
+                        onclick={() => { window.location.href = '/profile' }}
+                    />
+                    <Button
+                        label='Logout'
+                        class='px-4 py-1'
+                    />
+                </div>
+            </div>
         </div>
     </>
 }
