57c0d42597e115be4b8f2c362cdf4b632af97d63
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-30 23:43:39 +0700

    Enhance register page

--- src/components/register/Index.tsx
+++ src/components/register/Index.tsx
@@ -24,54 +24,58 @@ export default class Index extends React.Component<Props, State> {
         return <>
             {this.props.redirect}
             <form 
-                className='fixed left-1/2 -translate-x-1/2 bg-white top-1/2 -translate-y-1/2 w-1/3 flex flex-col items-center rounded-lg py-10 space-y-3'
+                className='fixed left-1/2 -translate-x-1/2 bg-white top-1/2 -translate-y-1/2 w-1/3 rounded-lg py-10'
             >
-                <h1 
-                    className='text-2xl font-semibold text-cyan-500'
+                <center
+                    className='space-y-3'
                 >
-                    Register Page
-                </h1>
-                <Input 
-                    type='text' 
-                    placeholder='First Name' 
-                    class='w-2/3 px-4 py-2' 
-                />
-                <Input 
-                    type='text' 
-                    placeholder='Last Name' 
-                    class='w-2/3 px-4 py-2' 
-                />
-                <Input 
-                    type='text' 
-                    placeholder='Email' 
-                    class='w-2/3 px-4 py-2' 
-                />
-                <Input 
-                    type='password' 
-                    placeholder='Password' 
-                    class='w-2/3 px-4 py-2'
-                />
-                <Input 
-                    type='password' 
-                    placeholder='Re-Password' 
-                    class='w-2/3 px-4 py-2'
-                />
-                <Button 
-                    type='submit' 
-                    label='Register' 
-                    class='w-2/3 py-2' 
-                />
-                <p
-                    className='font-semibold'
-                >
-                    Have an account? 
-                    <a 
-                        href='/login'
-                        className='ml-1 text-cyan-500 hover:text-cyan-600'
+                    <h1 
+                        className='text-2xl font-semibold text-cyan-500'
                     >
-                        Login
-                    </a>
-                </p>
+                        Register Page
+                    </h1>
+                    <Input 
+                        type='text' 
+                        placeholder='First Name' 
+                        class='w-2/3 px-4 py-2' 
+                    />
+                    <Input 
+                        type='text' 
+                        placeholder='Last Name' 
+                        class='w-2/3 px-4 py-2' 
+                    />
+                    <Input 
+                        type='text' 
+                        placeholder='Email' 
+                        class='w-2/3 px-4 py-2' 
+                    />
+                    <Input 
+                        type='password' 
+                        placeholder='Password' 
+                        class='w-2/3 px-4 py-2'
+                    />
+                    <Input 
+                        type='password' 
+                        placeholder='Re-Password' 
+                        class='w-2/3 px-4 py-2'
+                    />
+                    <Button 
+                        type='submit' 
+                        label='Register' 
+                        class='w-2/3 py-2' 
+                    />
+                    <p
+                        className='font-semibold'
+                    >
+                        Have an account? 
+                        <a 
+                            href='/login'
+                            className='ml-1 text-cyan-500 hover:text-cyan-600'
+                        >
+                            Login
+                        </a>
+                    </p>
+                </center>
             </form>
         </>
     }
