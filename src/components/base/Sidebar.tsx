44c4df79cffd07885e3452e4661d9afe3c7e1194
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-04-30 13:49:43 +0700

    Layouting sidebar

--- src/components/base/Sidebar.tsx
+++ src/components/base/Sidebar.tsx
@@ -5,7 +5,32 @@ export default function Sidebar() {
         <div 
             className='bg-white rounded-lg w-[20%] absolute top-20 h-[calc(100%-5rem)]'
         >
-            Sidebar
+            <ul>
+                <Item label='Fitur 1'/>
+                <Item label='Fitur 2'/>
+                <Item label='Fitur 3'/>
+                <Item label='Fitur 4'/>
+                <Item label='Fitur 5'/>
+            </ul>
         </div>
+    </>
+}
+
+interface Props {
+    label: string
+    href?: string
+}
+
+function Item(props: Props) {
+    return <>
+        <a 
+            href={props.href === undefined ? '#' : props.href}
+        >
+            <li
+                className='w-full border-b text-cyan-500 hover:bg-cyan-300 hover:text-white px-4 py-2 text-lg font-semibold'
+            >
+                {props.label}
+            </li>
+        </a>
     </>
 }
