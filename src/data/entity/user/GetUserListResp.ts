4a621879d0b03de9c5019208927a80c3a974c29b
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-03 22:32:15 +0700

    Update interface get user list

--- src/data/entity/user/GetUserListResp.ts
+++ src/data/entity/user/GetUserListResp.ts
@@ -1,15 +1,5 @@
-import { BaseResp } from "../BaseResp"
+import { BaseResp, UserResp } from "../BaseResp"
 
 export interface GetUserListResp extends BaseResp {
-    result: UserList[]
-}
-
-export interface UserList {
-    id: number
-    firstName: string
-    lastName: string
-    email: string
-    password: string
-    createdAt: string
-    updatedAt: string
+    result: UserResp[]
 }
