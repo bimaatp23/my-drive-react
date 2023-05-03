8fe9ecff452a6376085eed02866d293c8f0cdd98
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-03 23:46:31 +0700

    Update public constant

--- src/PublicConstant.ts
+++ src/PublicConstant.ts
@@ -1,2 +1,8 @@
 export const API_BASE_ENDPOINT = 'http://localhost:8000/api'
 export const API_USER_ENDPONT = 'user'
+
+export const HEADERS_MULTIPART = {
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    }
+}
