fad8053a1cfc201e386297cb0bdc09c0b0f622b2
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-06 17:02:20 +0700

    Update public contant

--- src/PublicConstant.ts
+++ src/PublicConstant.ts
@@ -1,5 +1,6 @@
 export const API_BASE_ENDPOINT = 'http://localhost:8000/api'
 export const API_USER_ENDPONT = 'user'
+export const API_TOKEN_ENDPONT = 'token'
 
 export const HEADERS_MULTIPART = {
     headers: {
