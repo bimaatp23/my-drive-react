0b1761bf670b027768ae30813feef3d5aa50d224
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-07 03:24:57 +0700

    Add check token service

--- src/data/service/TokenService.ts
+++ src/data/service/TokenService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Observable } from "rxjs";
-import { API_BASE_ENDPOINT, API_TOKEN_ENDPONT, HEADERS_MULTIPART } from "../../PublicConstant";
+import { API_BASE_ENDPOINT, API_TOKEN_ENDPONT, HEADERS_MULTIPART, HEADERS_TOKEN } from "../../PublicConstant";
 import { BaseResp } from "../entity/BaseResp";
 import { CreateTokenReq } from "../entity/token/CreateTokenReq";
 import { CreateTokenResp } from "../entity/token/CreateTokenResp";
@@ -14,6 +14,15 @@ export function CreateTokenService(createTokenReq: CreateTokenReq): Observable<B
                 observer.next(convertToCamelCase(response.data) as CreateTokenResp)
                 observer.complete()
             })
+            .catch(error => {
+                observer.error(convertToCamelCase(error.response.data) as BaseResp)
+            })
+    })
+}
+
+export function CheckTokenService(): Observable<BaseResp> {
+    return new Observable(observer => {
+        axios.get(`${API_BASE_ENDPOINT}/${API_TOKEN_ENDPONT}/check`, HEADERS_TOKEN)
             .catch(error => {
                 observer.error(convertToCamelCase(error.response.data) as BaseResp)
             })
