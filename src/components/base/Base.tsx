c4f8b5aae08f7993752e416c5dc9f7d8affa02d8
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-05-08 15:33:27 +0700

    Add auto logout

--- src/components/base/Base.tsx
+++ src/components/base/Base.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useEffect } from 'react'
 import { CheckTokenService } from '../../data/service/TokenService'
 import Header from './Header'
 import Sidebar from './Sidebar'
+import React from 'react'
 
 interface Props {
     element: ReactElement
@@ -22,6 +23,7 @@ export default function Base(props: Props): ReactElement {
             >
                 {props.element}
             </div>
+            <Timeout/>
         </div>
     </>
 }
@@ -36,4 +38,54 @@ function checkToken(): void {
                 }
             }
         })
+}
+
+interface P {
+
+}
+
+interface S {
+    time: number
+}
+class Timeout extends React.Component<P, S> {
+    constructor(props: P) {
+        super(props)
+        this.state = {
+            time: 5*60
+        }
+    }
+    componentDidMount(): void {
+        setInterval(() => {
+            if (this.state.time < 0) {
+                CheckTokenService()
+                .subscribe({
+                    error: error => {
+                        if (error.code === 403) {
+                            sessionStorage.clear() 
+                            window.location.assign('/login')
+                        }
+                    }
+                })
+            } else {
+                this.setState({
+                    time: this.state.time - 0.5
+                })
+            }
+        }, 1000)
+    }
+    numberToTime(seconds: number): string {
+        const min = Math.floor(seconds / 60)
+        const sec = Math.floor((seconds - (min * 60)) % 60)
+        const timeString = `${min < 10 ? '0' + min : min} : ${sec < 10 ? '0' + sec : sec}`
+        return timeString
+    }
+    render() {
+        return <>
+        <p
+            className='fixed left-2 bottom-1 text-cyan-600'
+        >
+            { this.state.time >= 0 ? this.numberToTime(this.state.time) : 'Timeout' }
+        </p>
+    </>
+    }
 }
